c56e83850c93b1a36c1e1fd281635103eee4fdad
fathimadiyam <160824356+fathimadiyam@users.noreply.github.com>
2024-07-15 14:54:09 +0530

    Add files via upload

---
+++ database/register.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:proj1/home.dart';
+import 'package:proj1/main.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => register(),
+      //   '/register': (context) => RegisterPage(),
+      // },
+    );
+  }
+}
+
+class register extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<register> {
+  bool isChecked = false;
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _passwordController2 =TextEditingController();
+  final DBbox=Hive.box('DBbox');
+  void write(var email,var pass){
+    DBbox.put(email, [email,pass]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String? e,p;
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Register Here!',
+                  style: TextStyle(fontFamily:AutofillHints.birthday,fontWeight: FontWeight.w500, color: Colors.green),
+                  textAlign: TextAlign.left,
+                  
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Enter your details',
+                  style: TextStyle(
+                    fontFamily: 'AutofillHints.birthdayDay',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 26),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                 TextFormField(
+                  controller: _passwordController2,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Re-enter your password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      e = _emailController.text.toString();
+                      p = _passwordController.text.toString();
+                    });
+                    write(e, p);
+                    // Navigate to home page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => homepage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  child: Container( width: double.infinity,
+                  child: Text('Register',textAlign: TextAlign.center,),)
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage()
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  child: Container( width: double.infinity,
+                  child: Text('Back',textAlign: TextAlign.center,),),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
+
